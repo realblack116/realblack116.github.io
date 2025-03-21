facecdd5feaf3c52e6fe5b0796432256303f3dc9
임동관 <realblack116@gmail.com>
2025-03-21 14:06:41 +0900

    Update main.py

--- gesture_control_system/main.py
+++ gesture_control_system/main.py
@@ -1,1 +1,9 @@
+pyimport sys
+from PySide6.QtWidgets import QApplication
+from gui.main_window import GestureControlSystem
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GestureControlSystem()
+    window.show()
+    sys.exit(app.exec())
