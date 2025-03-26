f1032d6c0eed8a7e9eee54025149576574266b1d
임동관 <realblack116@gmail.com>
2025-03-26 16:44:11 +0900

    Update urls.py

--- django/ecommerce_site/products/urls.py
+++ django/ecommerce_site/products/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    # ✅ 이 줄 추가
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
