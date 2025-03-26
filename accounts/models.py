3a8202234a485dc080138dac9f37f8515fb766a8
임동관 <realblack116@gmail.com>
2025-03-26 15:58:14 +0900

    Delete accounts/models.py

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    # 사용자에게 확장 필드가 필요하면 여기에 추가하세요
-    # 예: phone = models.CharField(max_length=20, blank=True)
-    pass
